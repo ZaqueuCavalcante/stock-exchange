ccf06c12c5159db22915e887b81b5a35c5686e7e
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-04-09 21:18:38 -0300

    Just save

--- Back/MatchingEngine/OrderBook.cs
+++ Back/MatchingEngine/OrderBook.cs
@@ -1,5 +1,15 @@
 namespace Xtk.Back.MatchingEngine;
 
+/// <summary>
+/// An order book is a list of buy and sell orders for a specific
+/// security or financial instrument, organized by price level.
+/// Operations:
+/// - Getting volume at a price level or between price levels.
+/// - Placing a new order, canceling an order, and matching an order.
+/// - Replacing an order.
+/// - Query best bid/ask.
+/// - Iterate through price levels.
+/// </summary>
 public class OrderBook
 {
     public Book BuyBook { get; set; }
